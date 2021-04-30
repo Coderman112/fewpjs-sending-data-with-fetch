cdd756256423a6e5ceea58a4d0e7f7cd4fa76418
ChristianCudby <christiancudby@hotmail.com>
2021-04-30 05:42:24 -0400

    commit tree 88327fdcf2deb57e2c9a19bfd8fe517c371dd451

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    };
+      
+      // method: "POST" is missing from the object below
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          console.log(object)
+          let h2 = document.createElement('h2');
+          h2.innerHTML = object.id;
+          document.body.appendChild(h2);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+
+}
+// let formData = {
+//     name: "Steve",
+//     email: "steve@steve.com"
+// };
+  
+//   // method: "POST" is missing from the object below
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+//     fetch("http://localhost:3000/users", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object)
+//       let h2 = document.createElement('h2');
+//       h2.innerHTML = object.id;
+//       document.body.appendChild(h2);
+//     })
+//     .catch(function(error) {
+//         let h3 = document.createElement('h3');
+//         h3.innerHTML = error.message;
+//         document.body.appendChild(h3);
+//         console.log(error.message);
+//     });
+    
+// }
+
